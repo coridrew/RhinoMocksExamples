df64e5bba4588d8bafb2de6fb8a7343b18c324f1
Sharon Cichelli <sharon@invisible-city.com>
2011-05-27 16:00:43 -0500

    stubs don't verify expectations but do assert method calls

--- src/RhinoMocksExamples/010_Difference_between_mocks_and_stubs.cs
+++ src/RhinoMocksExamples/010_Difference_between_mocks_and_stubs.cs
@@ -4,9 +4,11 @@ using RhinoMocksExamples.SystemUnderTest;
 
 namespace RhinoMocksExamples
 {
-    [TestFixture, Description("The InteractingClass is the class under test in this fixture.")]
+    [TestFixture]
     public class Difference_between_mocks_and_stubs
     {
+        //The InteractingClass is the class under test in this fixture.
+
         [Test]
         public void Mocks_are_semantically_important_to_the_test()
         {
@@ -63,6 +65,61 @@ namespace RhinoMocksExamples
             const string extraParameter = "my string";
             mock.Property = "my string";
             Assert.That(mock.Property, Is.Null);
+        }
+
+        [Test]
+        public void Only_mocks_verify_their_expectations()
+        {
+            //This is a legacy point. I recommend the AssertWasCalled syntax over the VerifyAll.
+            var mock = MockRepository.GenerateMock<ISampleInterface>();
+            mock.Expect(m => m.MethodThatReturnsInteger(Arg<string>.Is.Anything)).Return(4);
+            var interactor = new InteractingClass();
+            interactor.IgnoreTheInterface(mock);
+
+            //This should (and does) throw an expectation violation. Correctly failing test.
+            mock.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void Stubs_do_not_verify_their_expectations()
+        {
+            //This is a legacy point. I recommend the AssertWasCalled syntax over the VerifyAll.
+            var stub = MockRepository.GenerateStub<ISampleInterface>();
+            stub.Expect(s => s.MethodThatReturnsInteger(Arg<string>.Is.Anything)).Return(4);
+            var interactor = new InteractingClass();
+            interactor.IgnoreTheInterface(stub);
+
+            //This will silently pass, as if InteractingClass were meeting your expectations. 
+            //Falsely passing test.
+            stub.VerifyAllExpectations(); 
+        }
+
+        [Test]
+        public void Stubs_now_apply_AssertWasCalled()
+        {
+            //In prior versions of Rhino Mocks, AssertWasCalled and AssertWasNotCalled on 
+            //stubs would silently pass.
+            var stub = MockRepository.GenerateStub<ISampleInterface>();
+            var interactor = new InteractingClass();
+            interactor.IgnoreTheInterface(stub);
+
+            //This should (and does) throw an expectation violation in 3.5+. 
+            //Correctly failing test.
+            stub.AssertWasCalled(s => s.MethodThatReturnsInteger(Arg<string>.Is.Anything));
+        }
+
+        [Test]
+        public void Stubs_now_apply_AssertWasNotCalled()
+        {
+            //In prior versions of Rhino Mocks, AssertWasCalled and AssertWasNotCalled on 
+            //stubs would silently pass.
+            var stub = MockRepository.GenerateStub<ISampleInterface>();
+            var interactor = new InteractingClass();
+            interactor.AddWithTheInterface(stub);
+
+            //This should (and does) throw an expectation violation in 3.6. 
+            //Correctly failing test.
+            stub.AssertWasNotCalled(s => s.MethodThatReturnsInteger(Arg<string>.Is.Anything));
         }
     }
 }
